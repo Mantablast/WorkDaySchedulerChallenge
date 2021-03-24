80a08b83a3cb5c55b232d47d2ee1e70960fa6e66
AimeeJay <ajesso@post.com>
2021-03-23 22:43:55 -0400

    text block id's created, styles created for conditional color formatting

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -5,7 +5,7 @@ var formEl = document.querySelector("#task-form");
 var todaysDate = moment().format('dddd, MMMM Do YYYY');
 $("#currentDay").html(todaysDate);
 // document.todaysDate.setAttribute("class", "blue-text");
-
+console.log(moment().format('kk'));
 
 
 var loadTasks = function() {
@@ -25,7 +25,9 @@ if (!scheduleItems) {
 
 //a function to create tasks completely new or when loaded from the object arrays, also needs to set item to local storage
 
+// function saveTask {
 
+// }
 
 
 
@@ -62,75 +64,10 @@ if (!scheduleItems) {
 
 //ignore everything below***********
 
-
-//A function to receive tasks from local storage and feed them through a task element creator on their respective time block
-
-
-// var createSched = function(schedInfo, schedDate) {
-//     // create elements that make up a task item
-//     var taskLi = $("<li>").addClass("list-group-item");
-  
-//     var taskSpan = $("<span>")
-//       .addClass("badge badge-primary badge-pill")
-//       .text(taskDate);
-  
-//     var taskP = $("<p>")
-//       .addClass("m-1")
-//       .text(taskText);
-  
-//     // append span and p element to parent li
-//     taskLi.append(taskSpan, taskP);
-  
-  
-//     // append to ul list on the page
-//     $("#list-" + taskList).append(taskLi);
-//   };
-
-
-
-
-// audit task due dates every 30 minutes
-// setInterval(function() {
-//   $(".card .list-group-item").each(function() {
-//     auditTask($(this));
-//   });
-// }, 1800000);
-
-
-
-
-
-// function createSchedEl {
-//     var scedInfoEl = document.createElement("div");
-//   schedInfoEl.className = "task-info";
-//   schedInfoEl.innerHTML =
-//     "<h3 class='task-name'>" + schedDataObj.content + "</h3><span class='task-type'>" + schedDataObj.date + "</span>";
-//   listItemEl.appendChild(taskInfoEl);
-// }
-
-
-
-
-
-// var saveSched = function() {
-//     localStorage.setItem("schedule", JSON.stringify(schedule)); //tasks
-//   };
-
-// var loadSched = function() {
-//     var savedSched = localStorage.getItem("tasks");
-//     // if there are no tasks, set tasks to an empty array and return out of the function
-//     if (!savedSched) {
-//       return false;
-//     }
-//     console.log("A schedule exists!");
-//     // else, load up saved tasks
-  
-//     // parse into array of objects
-//     savedSched = JSON.parse(savedSched);
-  
-//     // loop through savedTasks array
-//     for (var i = 0; i < savedSched.length; i++) {
-//       // pass each task object into the `createTaskEl()` function
-//       createTaskEl(savedSched[i]);  //was savedtasks
-//     }
-//   };
+// 1. Add a class to each of your text areas (same one)
+// 2. add a UNIQUE id to each timebloack for the hour it is
+// 3. Add a class (or use one that you already have for the bootstrap stuff) to the button
+// 4. use a onclick (or click in jquery) on the button class you are using.
+// 5. in the function that starts on the click use the JQUERY 'this' to grab the specific timeblock div
+// 6. Jquery has ways to grab siblings, parents, children etc and you can get that text area and the unique ID.
+// 7. save to local storage using the unique id as the key and text area value as the value
