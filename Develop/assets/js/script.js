01917517ba87fe4741014a39a179aae82814a7eb
AimeeJay <ajesso@post.com>
2021-03-25 00:15:46 -0400

    code cleanup

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,5 +1,3 @@
-
-
 //Retreiving the current day of week and month 
 var todaysDate = moment().format('dddd, MMMM Do YYYY');
 $("#currentDay").html(todaysDate);
@@ -28,41 +26,23 @@ $(".time-block").each(function () {
       $(this).addClass("green-future");
     }
   })
-  // loadTasks();
+  loadTasks();
 };
 
-// var loadTasks = function() {
-//     getSchedItems = JSON.parse(localStorage.getItem("scheduleItems"));
-// // if nothing in localStorage, create a new object to track all task status arrays
-// if (!scheduleItems) {
-//     scheduleItems = {
-//       scheduleContent: [],
-//     };
-//   }
-// else ()
-//   // loop over object properties to insert to correct places
-// };
-
-
-//a function to create tasks completely new or when loaded from the object arrays, also needs to set item to local storage
-// $("#task-form").on("click", "p", function() {
-//   var text = $(this)
-//     .text()
-//     .trim();
-//     $(this).replaceWith(textInput);
-//     textInput.trigger("focus");
-//     // recreate p element
-// var taskP = $("<p>")
-// .addClass("m-1")
-// .text(text);
-
-// // replace textarea with p element
-// $(this).replaceWith(taskP);
-// });
-
-// }
-
-
+var loadTasks = function() {
+    getSchedItems = JSON.parse(localStorage.getItem("scheduleContent"));
+    // if nothing in localStorage, create a new object to track all task status arrays
+    if (!scheduleItems) {
+    scheduleItems = {
+      hourId: [],
+      scheduleItems: []
+      }
+      }
+      else {  $(".time-block").each(function() {
+      $("#09 .text-box-insert").val(localStorage.getItem("09"));
+      console.log("09:00 content retrieved");
+   }
+)};
 
 
 //create a time interval setinterval() that will update content with current hour and trigger an hour tracking function
@@ -71,8 +51,6 @@ setInterval(function() {
 }, 1800000);
 
 colorCoding();
-
-
 
 
 
@@ -86,35 +64,4 @@ $( ".save-icon" ).click(function() {
 
   console.log(hourId);
   scheduleContent = window.localStorage.setItem(`${hourId}`, JSON.stringify(scheduleContent));
-});
-
-// var scheduleContent = $(this).parent().siblings()[1].child()//this isnt working
-// var hourId = $(this).samethignbuttogettheid
-//  scheduleContent = window.localStorage.setItem(`${hourId`, JSON.stringify(scheduleContent));
-
-//not yet working
-// function clearAll() {
-//   var clearAsk = prompt("Are you sure?  The tasks can no longer be retrieved after clearing.")
-//   if(!clearAsk) {
-//     console.log("cancel clear");
-//   }
-//   else {
-//     console.log("initiate clear task sequence");
-//   }
-// };
-
-// document.getElementsByClassName("clear-all").addEventListener("click", clearAll);
-
-
-
-
-
-//ignore everything below***********
-
-// 1. Add a class to each of your text areas (same one) done
-// 2. add a UNIQUE id to each timebloack for the hour it is done
-// 3. Add a class (or use one that you already have for the bootstrap stuff) to the button done!
-// 4. use a onclick (or click in jquery) on the button class you are using.
-// 5. in the function that starts on the click use the JQUERY 'this' to grab the specific timeblock div
-// 6. Jquery has ways to grab siblings, parents, children etc and you can get that text area and the unique ID.
-// 7. save to local storage using the unique id as the key and text area value as the value
+})
